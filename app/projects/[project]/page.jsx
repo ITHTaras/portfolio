639b2a94b367962d03ead55d113d5e5bdd9765fc
Taras Hornik <tarasgorniko@gmail.com>
2024-07-27 15:57:03 +0200

    second picture

--- app/projects/[project]/page.jsx
+++ app/projects/[project]/page.jsx
@@ -58,7 +58,7 @@ function project({ params }) {
           </div>
         </div>
         <div className="mt-10 h-[240vh] md:h-[100vh] md:flex md:gap-5 md:justify-between">
-          <div className="max-md: mt-4 relative h-[80vh] w-full">
+          <div className="max-md:mt-4 relative h-[80vh] w-full">
             <Image
               className="object-contain"
               fill
@@ -66,7 +66,7 @@ function project({ params }) {
               src="/images/ecolife_1.jpg"
             />
           </div>
-          <div className="max-md: mt-4 relative h-[80vh] w-full">
+          <div className="max-md:mt-4 relative h-[80vh] w-full">
             <Image
               className="object-contain"
               fill
@@ -74,7 +74,7 @@ function project({ params }) {
               src="/images/ecolife_2.jpg"
             />
           </div>
-          <div className="max-md: mt-4 relative h-[80vh] w-full">
+          <div className="max-md:mt-4 relative h-[80vh] w-full">
             <Image
               className="object-contain"
               fill
@@ -97,8 +97,8 @@ function project({ params }) {
   } else if (project && project.alias === "intrusion-alarm") {
     return (
       <div className="px-8 md:px-20">
-        <div className="md:grid md:grid-cols-12 md:gap-4 md:h-[80vh]">
-          <div className="relative w-full col-span-5">
+        <div className="md:grid md:grid-cols-12 md:gap-4 lg:h-[80vh]">
+          <div className="relative max-md:h-[70vh] w-full col-span-5">
             <Image
               className="object-contain h-[auto!important] max-h-full"
               src={project.img}
@@ -110,29 +110,44 @@ function project({ params }) {
             {lang == "de" ? (
               <p className="text-slate-200 mt-3 lg:w-[500px]">
                 Ein Einbruchsalarm überprüft ständig die Lage des Reed-Kontaktes
-                und schaltet dementsprechend ein. Zum Ausschalten muss eine
-                vierstellige Kennnummer mithilfe eines Infrarotpults eingegeben
-                werden. Während der Eingabe werden die Kennziffern auf einem
-                LCD-Bildschirm abgebildet, der mithilfe eines transistorisierten
-                Logikgatters ein- oder ausgeschaltet wird. Die Komponenten
-                werden durch einen Arduino UNO R3 Mikrocontroller gesteuert.
-                Mehr Informationen über die einzelnen Bauteile finden Sie in
-                einem Ordner mit dem Namen "include" in meinem
-                GitHub-Repository, dessen Link unten auf der Website zu finden
-                ist.
+                und schaltet dementsprechend ein. Externe Bauteile (der Summer,
+                Transistor eines logischen Gatters, das Potentiometer und
+                dazugehörige Festwiderstände) wurden auf einer Lochrasterplatte
+                gelötet. Zum Ausschalten muss eine vierstellige Kennnummer
+                mithilfe eines Infrarotpults eingegeben werden. Während der
+                Eingabe werden die Kennziffern auf einem LCD-Bildschirm
+                abgebildet, der mithilfe des transistorisierten Logikgatters
+                ein- oder ausgeschaltet wird. Die Komponenten werden durch einen
+                Arduino UNO R3 Mikrocontroller gesteuert. Mehr Informationen
+                über die einzelnen Bauteile finden Sie in einem Ordner mit dem
+                Namen "include" in meinem GitHub-Repository, dessen Link unten
+                auf der Website zu finden ist.
               </p>
             ) : (
               <p className="text-slate-200 mt-3 lg:w-[500px]">
                 An intrusion alarm continuously checks the state of a Reed
-                sensor. By inputting a code consisting of four digits using an
-                infrared controller, the alarm can be switched off. The state of
-                the input is displayed on an LCD screen, which will be turned
-                off when not needed, using transistor logic. This project uses
-                an Arduino UNO R3 microcontroller. You will find more details
-                about the components in the image in the /include folder in my
-                GitHub repository, which is linked in the footer of the website.
+                sensor. Some components (passive buzzer, transistor of a logic
+                gate, potentiometer and corresponding fixed resistors) were
+                soldered onto a perfboard. By inputting a code consisting of
+                four digits using an infrared controller, the alarm can be
+                switched off. The state of the input is displayed on an LCD
+                screen, which will be turned off when not needed, using
+                transistor logic. This project uses an Arduino UNO R3
+                microcontroller. You will find more details about the components
+                in the image in the /include folder in my GitHub repository,
+                which is linked in the footer of the website.
               </p>
             )}
+          </div>
+        </div>
+        <div className="mt-10 h-[240vh] md:h-[100vh] md:flex md:gap-5 md:justify-between">
+          <div className="max-md:mt-4 relative h-[80vh] w-full">
+            <Image
+              className="object-contain"
+              fill
+              alt=""
+              src="/images/intrusion-alarm_final.jpg"
+            />
           </div>
         </div>
       </div>
